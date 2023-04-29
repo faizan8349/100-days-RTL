986add319900f9f5dc4743be40b2f94c1e928459
faizan8349 <131616660+faizan8349@users.noreply.github.com>
2023-04-29 15:32:12 +0530

    Create design.sv

---
+++ Day1/design.sv
@@ -0,0 +1,22 @@
+// Code your design here
+class c;
+  
+static  reg [3:0] a;
+  
+  
+
+
+  
+endclass
+
+module m;
+  
+  c c1=new();
+  
+  initial 
+    begin 
+      
+      c1.a=1;
+      
+    end 
+endmodule 
