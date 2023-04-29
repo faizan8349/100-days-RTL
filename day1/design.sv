d583301954924671c0b39de49d53673e4a4ee7f3
faizan8349 <131616660+faizan8349@users.noreply.github.com>
2023-04-29 15:36:55 +0530

    Update design.sv

--- day1/design.sv
+++ day1/design.sv
@@ -1,1 +1,21 @@
 
+# 100-days-RTL
+
+//HALF ADDER USING GATE LEVEL
+
+module halfadder(a,b,c,s);
+
+input a,b;
+
+output s,c;
+
+wire w1,w2,w3,w4; 
+
+ not (w1,a);
+  not (w2,b);
+  and (w3,w1,b);
+  and (w4,w2,a);
+  or (s,w2,w4);  
+  and  (c,a,b);
+
+endmodule
